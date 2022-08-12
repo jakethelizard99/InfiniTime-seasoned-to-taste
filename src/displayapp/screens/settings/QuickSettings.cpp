4983297878a9d659554c78a1a6a0e642b2f3e9c4
mashuptwice <info@mashup-tech.de>
2022-08-12 16:05:50 +0200

    adjusted quicksettings button color

--- src/displayapp/screens/settings/QuickSettings.cpp
+++ src/displayapp/screens/settings/QuickSettings.cpp
@@ -54,7 +54,7 @@ QuickSettings::QuickSettings(Pinetime::Applications::DisplayApp* app,
 
   lv_style_init(&btn_style);
   lv_style_set_radius(&btn_style, LV_STATE_DEFAULT, buttonHeight / 4);
-  lv_style_set_bg_color(&btn_style, LV_STATE_DEFAULT, LV_COLOR_MAKE(0x38, 0x38, 0x38));
+  lv_style_set_bg_color(&btn_style, LV_STATE_DEFAULT, lv_color_hex(0x292929));
 
   btn1 = lv_btn_create(lv_scr_act(), nullptr);
   btn1->user_data = this;
