8aabf16fc78b43186651dbdfbb3addc1bf7f8542
jakethelizard99 <87454330+jakethelizard99@users.noreply.github.com>
2022-08-13 17:12:33 -0500

    Update List.cpp

--- src/displayapp/screens/List.cpp
+++ src/displayapp/screens/List.cpp
@@ -46,7 +46,7 @@ List::List(uint8_t screenID,
       itemApps[i] = lv_btn_create(container1, nullptr);
       lv_obj_set_style_local_bg_opa(itemApps[i], LV_BTN_PART_MAIN, LV_STATE_DEFAULT, LV_OPA_50);
       lv_obj_set_style_local_radius(itemApps[i], LV_BTN_PART_MAIN, LV_STATE_DEFAULT, 57);
-      lv_obj_set_style_local_bg_color(itemApps[i], LV_BTN_PART_MAIN, LV_STATE_DEFAULT, LV_COLOR_Hex(#00e24a));
+      lv_obj_set_style_local_bg_color(itemApps[i], LV_BTN_PART_MAIN, LV_STATE_DEFAULT, LV_COLOR_Hex(0x00e24a));
 
       lv_obj_set_width(itemApps[i], LV_HOR_RES - 8);
       lv_obj_set_height(itemApps[i], 57);
