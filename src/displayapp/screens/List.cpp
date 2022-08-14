6ced919fa2bcf9cd0b7f0a3d6a342e1dd172a402
jakethelizard99 <87454330+jakethelizard99@users.noreply.github.com>
2022-08-13 22:35:25 -0500

    Update List.cpp

--- src/displayapp/screens/List.cpp
+++ src/displayapp/screens/List.cpp
@@ -46,7 +46,7 @@ List::List(uint8_t screenID,
       itemApps[i] = lv_btn_create(container1, nullptr);
       lv_obj_set_style_local_bg_opa(itemApps[i], LV_BTN_PART_MAIN, LV_STATE_DEFAULT, LV_OPA_50);
       lv_obj_set_style_local_radius(itemApps[i], LV_BTN_PART_MAIN, LV_STATE_DEFAULT, 57);
-      lv_obj_set_style_local_bg_color(itemApps[i], LV_BTN_PART_MAIN, LV_STATE_DEFAULT, LV_COLOR_MAKE(0x0, 0xE2, 0x4A));
+      lv_obj_set_style_local_bg_color(itemApps[i], LV_BTN_PART_MAIN, LV_STATE_DEFAULT, LV_COLOR_MAKE(0x00, 0xE2, 0x4A));
 
       lv_obj_set_width(itemApps[i], LV_HOR_RES - 8);
       lv_obj_set_height(itemApps[i], 57);
@@ -55,7 +55,7 @@ List::List(uint8_t screenID,
       itemApps[i]->user_data = this;
 
       labelBtIco = lv_label_create(itemApps[i], nullptr);
-      lv_obj_set_style_local_text_color(labelBtIco, LV_LABEL_PART_MAIN, LV_STATE_DEFAULT, LV_COLOR_GRAY);
+      lv_obj_set_style_local_text_color(labelBtIco, LV_LABEL_PART_MAIN, LV_STATE_DEFAULT, LV_COLOR_WHITE);
       lv_label_set_text_static(labelBtIco, applications[i].icon);
 
       labelBt = lv_label_create(itemApps[i], nullptr);
